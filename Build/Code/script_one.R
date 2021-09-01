c8a51e4c82f1b5745d7995da2ec4b5b2e0f17e01
ethanr2 <ethanrhmn@gmail.com>
2021-09-01 17:46:33 -0500

    Finished the HW1 assignment with the DIF function, graphs, and the pdf.

--- Build/Code/script_one.R
+++ Build/Code/script_one.R
@@ -7,46 +7,58 @@ rm(list=ls())
 library(tidyverse)
 library(readr)
 
-#Function####
-delta <- function(x){
-  temp<-((x - lag(x))/lag(x))
-  return(temp)
-}
 
+#Function####
+DIF <- function(x){
+  return(x - lag(x))
+}
 
 covidIL <- read_csv("Data/ILCovid19.csv", 
                       col_types = cols(Date = col_date(format = "%m/%d/%Y")))
 covidIL <- covidIL %>%
-  mutate(pc_cases = delta(Cases),
-         pc_tests = delta(Tests),
-         pc_deaths= delta(Deaths))
+  mutate(new_cases = DIF(Cases),
+         new_tests = DIF(Tests),
+         new_deaths= DIF(Deaths))
+covidIL <- covidIL %>%
+  mutate(pc_cases = new_cases/lag(Cases),
+         pc_tests = new_tests/lag(Tests),
+         pc_deaths= new_deaths/lag(Deaths))
+(covidIL)
 
-plot(covidIL$Date, 
-     covidIL$pc_cases,
-     main="Percent Cases",
-     xlab="",
-     ylab="",
-     type ="l")
+# plot(covidIL$Date, 
+#      covidIL$pc_cases,
+#      main="Percent Cases",
+#      xlab="",
+#      ylab="",
+#      type ="l")
 
-plot <-ggplot() +
-  geom_line(data= covidIL, 
-            aes(x = Date, 
-                y = pc_cases, 
-                color = 'cases')) +
-  geom_line(data=covidIL,
-            aes(x = Date, 
-                y = pc_tests, 
-                color = 'tests')) +
-  geom_line(data=covidIL,
-            aes(x = Date, 
-                y = pc_deaths, 
-                color = 'deaths')) +
-  scale_color_manual(values = c('tests' = 'blue',
-                                'cases' = 'orange',
-                                'deaths' = 'red')) + 
-  labs(title = 'Covid19', 
-       x = 'Date', 
+plot1 <-ggplot(data= covidIL, 
+               aes(x = Date, y = pc_cases, color = 'Red')) +
+  geom_line() + 
+  labs(title = 'Daily Percentage Change in New Cases',
+       x = 'Date',
+       y = 'Percentage Change',) + 
+  theme(legend.position = "none")
+show(plot1)
+
+plot2 <-ggplot(data= covidIL, 
+               aes(x = Date, y = pc_tests, color = 'Blue')) +
+  geom_line() + 
+  labs(title = 'Daily Percentage Change in Tests',
+       x = 'Date',
+       y = 'Percentage Change') + 
+  theme(legend.position = "none")
+show(plot2)
+
+plot3 <-ggplot(data= covidIL, 
+               aes(x = Date, y = pc_deaths, color = 'orange')) +
+  geom_line() + 
+  labs(title = 'Daily Percentage Change in Deaths',
+       x = 'Date',
        y = 'Percentage Change',
-       color = 'Time Series') + 
-  theme(legend.position = "right")
-show(plot)
+       ) + 
+  theme(legend.position = "none")
+
+show(plot3)
+#Daily Percentage Change in New Cases, 
+#Tests, Daily Percentage Change in Deaths
